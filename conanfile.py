a3177a927e13373836a42fe337cbe559241654a2
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:12 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostGilConan(ConanFile):
     name = "Boost.Gil"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/gil"
+    url = "https://github.com/bincrafters/conan-boost-gil"
+    source_url = "https://github.com/boostorg/gil"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "gil"
@@ -23,7 +23,7 @@ class BoostGilConan(ConanFile):
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
