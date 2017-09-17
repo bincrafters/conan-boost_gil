1434086187cb29dc9fd5ece9af2c607f1ba210b6
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:32:28 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,22 +2,22 @@ from conans import ConanFile, tools, os
 
 class BoostGilConan(ConanFile):
     name = "Boost.Gil"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-gil"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["gil"]
-    requires =  "Boost.Bind/1.64.0@bincrafters/testing", \
-                      "Boost.Concept_Check/1.64.0@bincrafters/testing", \
-                      "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Iterator/1.64.0@bincrafters/testing", \
-                      "Boost.Mpl/1.64.0@bincrafters/testing", \
-                      "Boost.Preprocessor/1.64.0@bincrafters/testing", \
-                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing",\
+    requires =  "Boost.Bind/1.65.1@bincrafters/testing", \
+                      "Boost.Concept_Check/1.65.1@bincrafters/testing", \
+                      "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Iterator/1.65.1@bincrafters/testing", \
+                      "Boost.Mpl/1.65.1@bincrafters/testing", \
+                      "Boost.Preprocessor/1.65.1@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Type_Traits/1.65.1@bincrafters/testing",\
 
                       #bind3 concept_check5 config0 core2 iterator5 mpl5 preprocessor0 smart_ptr4 static_assert1 type_traits3
 
--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-conan upload --all --remote bincrafters Boost.Gil/1.64.0@bincrafters/testing
+conan upload --all --remote bincrafters Boost.Gil/1.65.1@bincrafters/testing
