ba22e1743506d37e484f4dfe5b8cc752d83be0bb
Jerry W <jerrywiltse@gmail.com>
2017-12-31 18:18:25 -0500

    Update url field in conanfile [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conans import ConanFile, tools
 class BoostGilConan(ConanFile):
     name = "boost_gil"
     version = "1.66.0"
-    url = "https://github.com/bincrafters/conan-boost-gil"
+    url = "https://github.com/bincrafters/conan-boost_gil"
 
     lib_short_names = ["gil"]
     is_header_only = True
