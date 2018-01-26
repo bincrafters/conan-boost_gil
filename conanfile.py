7a0eaa3edf7de89ffb4619122ac44e3ebe6ee2ab
Jerry W <jerrywiltse@gmail.com>
2018-01-26 11:21:36 -0500

    switch deps to testing

--- conanfile.py
+++ conanfile.py
@@ -17,17 +17,17 @@ class BoostGilConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/stable",
-        "boost_bind/1.66.0@bincrafters/stable",
-        "boost_concept_check/1.66.0@bincrafters/stable",
-        "boost_config/1.66.0@bincrafters/stable",
-        "boost_core/1.66.0@bincrafters/stable",
-        "boost_iterator/1.66.0@bincrafters/stable",
-        "boost_mpl/1.66.0@bincrafters/stable",
-        "boost_preprocessor/1.66.0@bincrafters/stable",
-        "boost_smart_ptr/1.66.0@bincrafters/stable",
-        "boost_static_assert/1.66.0@bincrafters/stable",
-        "boost_type_traits/1.66.0@bincrafters/stable"
+        "boost_package_tools/1.66.0@bincrafters/testing",
+        "boost_bind/1.66.0@bincrafters/testing",
+        "boost_concept_check/1.66.0@bincrafters/testing",
+        "boost_config/1.66.0@bincrafters/testing",
+        "boost_core/1.66.0@bincrafters/testing",
+        "boost_iterator/1.66.0@bincrafters/testing",
+        "boost_mpl/1.66.0@bincrafters/testing",
+        "boost_preprocessor/1.66.0@bincrafters/testing",
+        "boost_smart_ptr/1.66.0@bincrafters/testing",
+        "boost_static_assert/1.66.0@bincrafters/testing",
+        "boost_type_traits/1.66.0@bincrafters/testing"
     )
 
     # BEGIN
@@ -35,7 +35,7 @@ class BoostGilConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/stable"
+    build_requires = "boost_generator/1.66.0@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
