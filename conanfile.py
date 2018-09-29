c97420cb0232b4e3459ccccca52b7436c5ee8ac1
Jerry W <jerrywiltse@gmail.com>
2018-09-29 17:15:30 -0400

    move to python_requires strategy

--- conanfile.py
+++ conanfile.py
@@ -1,67 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools
+from conans import python_requires
 
 
-class BoostGilConan(ConanFile):
+base = python_requires("boost_base/1.67.0@bincrafters/testing")
+
+class BoostGilConan(base.BoostBaseConan):
     name = "boost_gil"
-    version = "1.67.0"
-    author = "Bincrafters <bincrafters@gmail.com>"
-    exports = ["LICENSE.md"]
-    lib_short_names = ["gil"]
-    is_header_only = True
-
-    def package_id_additional(self):
-        self.info.header_only()
-
-    requires = (
-        "boost_bind/1.67.0@bincrafters/testing",
-        "boost_concept_check/1.67.0@bincrafters/testing",
-        "boost_config/1.67.0@bincrafters/testing",
-        "boost_core/1.67.0@bincrafters/testing",
-        "boost_iterator/1.67.0@bincrafters/testing",
-        "boost_mpl/1.67.0@bincrafters/testing",
-        "boost_package_tools/1.67.0@bincrafters/testing",
-        "boost_preprocessor/1.67.0@bincrafters/testing",
-        "boost_smart_ptr/1.67.0@bincrafters/testing",
-        "boost_static_assert/1.67.0@bincrafters/testing",
-        "boost_type_traits/1.67.0@bincrafters/testing"
-    )
-
-    # BEGIN
-
     url = "https://github.com/bincrafters/conan-boost_gil"
-    description = "Please visit http://www.boost.org/doc/libs/1_67_0"
-    license = "BSL-1.0"
-    short_paths = True
-    build_requires = "boost_generator/1.67.0@bincrafters/testing"
+    lib_short_names = ["gil"]
+    header_only_libs = ["gil"]
+    b2_requires = [
+        "boost_bind",
+        "boost_concept_check",
+        "boost_config",
+        "boost_core",
+        "boost_iterator",
+        "boost_mpl",
+        "boost_preprocessor",
+        "boost_smart_ptr",
+        "boost_static_assert",
+        "boost_type_traits"
+    ]
 
-    def package_id(self):
-        getattr(self, "package_id_additional", lambda:None)()
 
-    def source(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.source(self)
-        getattr(self, "source_additional", lambda:None)()
-
-    def build(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.build(self)
-        getattr(self, "build_additional", lambda:None)()
-
-    def package(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package(self)
-        getattr(self, "package_additional", lambda:None)()
-
-    def package_info(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package_info(self)
-        getattr(self, "package_info_additional", lambda:None)()
-
-    # END
