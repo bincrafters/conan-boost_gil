dea67b95ee76bf78abb38bae70c5ad762b499aba
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostGilConan(base.BoostBaseConan):
     name = "boost_gil"
@@ -12,16 +12,19 @@ class BoostGilConan(base.BoostBaseConan):
     lib_short_names = ["gil"]
     header_only_libs = ["gil"]
     b2_requires = [
+        "boost_algorithm",
         "boost_bind",
         "boost_concept_check",
         "boost_config",
         "boost_core",
+        "boost_filesystem",
+        "boost_function",
+        "boost_integer",
         "boost_iterator",
+        "boost_lambda",
         "boost_mpl",
+        "boost_numeric_conversion",
         "boost_preprocessor",
-        "boost_smart_ptr",
         "boost_static_assert",
         "boost_type_traits"
     ]
-
-
