f373fa6fbcc2c5c08402f5d4a219103dc6c20bc5
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:26 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,27 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostGilConan(base.BoostBaseConan):
     name = "boost_gil"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_gil"
-    lib_short_names = ["gil"]
-    header_only_libs = ["gil"]
-    b2_requires = [
-        "boost_bind",
-        "boost_concept_check",
-        "boost_config",
-        "boost_core",
-        "boost_filesystem",
-        "boost_function",
-        "boost_integer",
-        "boost_iterator",
-        "boost_lambda",
-        "boost_mpl",
-        "boost_numeric_conversion",
-        "boost_preprocessor",
-        "boost_static_assert",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
